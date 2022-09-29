b1507fe99730b896747230a02d7582cb03a73541
Kossychukwu <kossydike@gmail.com>
2022-09-29 22:38:48 +0100

    algorithm

---
+++ algorithm.js
@@ -0,0 +1,33 @@
+
+//algorith to find the number of vowels, the numbers of words, and the length of a sentence.
+
+function myFunction (str){
+
+  const strLength = (str) =>{const length = str.length 
+  return length
+  };
+  const strNumber = (str) => {
+ let words = str.split(" ");
+ let wordCount = 0;
+ for (let element of words){
+  wordCount++
+ }
+ return wordCount
+  }
+
+
+const vowelCount = (str) => {
+  const vowels = ["A" , "E", "I", "O", "U"];
+  let count = 0;
+ for (let element of str.toUpperCase()){
+  if (vowels.includes(element))
+  count++
+ }
+ return count
+
+}
+return `${ "vowels" + ":" + " " + vowelCount(str)}, ${ "words" + ":" + " " + strNumber(str)}, ${"length" + ":" + " " + strLength(str)}`
+}
+ 
+
+console.log(myFunction("i am the greatest")) // returns vowels : 6 , words: 5, length: 18;
